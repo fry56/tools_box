7e30130983759ad9716e64763fdc48a0dca65c42
fry <jules.danet@epitech.eu>
2023-02-15 15:45:46 +0100

    [Fix] - str_ncmp

--- src/string/str_cmp.c
+++ src/string/str_cmp.c
@@ -4,7 +4,7 @@
 ** File description:
 ** desc
 */
-
+#include <stdio.h>
 int tstr_cmp(char const *s1, char const *s2)
 {
     for (; *s1 || *s2 ; s1++, s2++) {
@@ -16,7 +16,7 @@ int tstr_cmp(char const *s1, char const *s2)
 
 int tstr_ncmp(char const *s1, char const *s2, int n)
 {
-    for (int i = 0; *s1 || *s2 || i > n; s1++, s2++, i++) {
+    for (int i = 0; (*s1 || *s2) && i < n; s1++, s2++, i++) {
         if (*s1 != *s2)
             return *s1 - *s2;
     }

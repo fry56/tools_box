409556205d9cd128429c2bf9185bb0dad95e8ca5
fry <jules.danet@epitech.eu>
2023-02-22 12:22:58 +0100

    [Fix] hashmap_remove if prendent node destroy value is now set to NULL.

--- src/hashmap/hashmap_remove.c
+++ src/hashmap/hashmap_remove.c
@@ -13,6 +13,7 @@ void remove_node_with_child(t_hashmap *hashmap, t_hashmap_node *node
     , const char *key)
 {
     t_list_node *child_node = node->childs_node->tail;
+
     if (hashmap == NULL || node == NULL || tstr_len(key) == 0)
         return;
     hashmap->child_elements--;
@@ -27,23 +28,22 @@ void remove_node_with_child(t_hashmap *hashmap, t_hashmap_node *node
 bool thashmap_remove(t_hashmap *hashmap, const char *key)
 {
     t_hashmap_node *node = thashmap_get(hashmap, key);
+
     if (node == NULL)
         return false;
-    if (node->parent_node) {
-        tlist_remove(node->parent_node->childs_node
-            , tlist_find(node->parent_node->childs_node, node));
-        node->host->child_elements--;
-        return true;
-    }
-    if (node->childs_node) {
+    if (node->parent_node == NULL) {
         if (node->childs_node->length > 0) {
             remove_node_with_child(hashmap, node, key);
         } else {
             node->host->node_elements--;
             free(node->childs_node);
         }
+        hashmap->map[node->hash] = NULL;
         free(node);
         return true;
     }
-    return false;
+    tlist_remove(node->parent_node->childs_node
+            , tlist_find(node->parent_node->childs_node, node->value));
+    node->host->child_elements--;
+    return true;
 }
